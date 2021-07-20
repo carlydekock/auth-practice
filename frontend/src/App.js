ad7a301efba53710af6c596478087ee1f18bdf77
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-20 16:11:52 -0700

    setup of buttons, login with redirect working on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+
+  const {
+    loginWithPopup, 
+    loginWithRedirect, 
+    logout, 
+    user, 
+    isAuthenticated 
+  } = useAuth0();
+
   return (
     <div className="App">
       <h1>Auth0 authentication</h1>
-      <ul
-      style={{
-        maxWidth: '500px',
-        listStyle: 'none',
-        margin: 'auto'
-      }}>
-        <li>Frontend- ReactJS</li>
-        <li>Backend - NodeJS Express API</li>
+      <ul style={{listStyle: "none"}}>
+        <li><button onClick={loginWithPopup}>Login with Popup</button></li>
+        <li><button onClick={loginWithRedirect}>Login with Redirect</button></li>
+        <li><button onClick={logout}>Logout</button></li>
       </ul>
+      <h3>User is { isAuthenticated ? "Logged in" : "Not logged in" } </h3>
+      { isAuthenticated && <pre style={{textAlign: 'start'}}>{JSON.stringify(user, null, 2)}</pre> }
+      
     </div>
   );
 }
